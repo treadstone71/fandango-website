03ffc04844c68e9af722aec9b879e35103ef6364
Sasank <sasank196@gmail.com>
2018-04-23 01:19:50 -0700

    Added Login page for Admin.

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { history } from './_helpers/history.js';
+import Login from './components/Logins/login.js';
+import HomePage from './components/homepage/HomePage';
+import { Router, Route, Link, Redirect } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Router history={history}>
+    <div>
+    <Route path="/adminlogin" component={Login}/>
+    <Route exact path="/" component={HomePage} />
+
+    </div>
+    </Router>
+    , document.getElementById('root'));
 registerServiceWorker();
