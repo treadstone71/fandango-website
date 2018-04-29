41011c3707ae1e8b6241a97deeb1517c33a91f89
samliweisen <liweisen007@gmail.com>
2018-04-29 02:05:19 +0000

    add movie list on home page

--- client/src/components/homepage/HomePage.js
+++ client/src/components/homepage/HomePage.js
@@ -10,7 +10,20 @@ class HomePage extends React.Component{
         return (
             <div class="container">
             <MainNav />
-
+                <div className="row page">
+                { [1,2,3,4].map((i) => 
+                    <div class="col">
+                    <div class="card">
+                        <img class="card-img-top" src="https://cdn.images.express.co.uk/img/dynamic/36/590x/secondary/Avengers-Infinity-War-ends-with-many-of-these-characters-dead-1322280.jpg" alt="Movie Title" />
+                        <div class="card-body">
+                            <h5 class="card-title">Movie title</h5>
+                            <p class="card-text">Movie Description</p>
+                            <a href="#" class="btn btn-primary">Buy Tickets</a>
+                        </div>
+                    </div>
+                    </div>
+                )}
+                </div>
             </div>
 
         );
