739e295c63a3488df31877f90530871cb806df09
Prashanth Rangaraju <prashanth.rangaraju@sjsu.edu>
2018-04-29 11:12:32 -0700

    added login in the backend

--- server/node_backend/login.js
+++ server/node_backend/login.js
@@ -1,17 +1,55 @@
 var loginMethods = {}
+var getConnection = require('./database/mysql');
+var getMongodb = require('./database/mongodb.js');
+var mysql = require('mysql'); 
+var cryptPassword = require('./crypto.js').cryptPassword;
+var comparePassword = require('./crypto.js').comparePassword;
+
+let adminusername = "admin";
+let adminpassword = "password";
 
 loginMethods.login = function(value, done){
 	var data = value.data;
-	if(data.username == "normaluser" && data.password == "pass"){
-		return done({correlationId: value.correlationId, replyTo: value.replyTo, data: {status: "SUCCESS", type:"user", username: data.username}});
-	}
-	if(data.username == "adminuser" && data.password == "pass"){
-		return done({correlationId: value.correlationId, replyTo: value.replyTo, data: {status: "SUCCESS", type:"admin", username: data.username}});
-	}
-	if(data.username == "moviehalluser" && data.password == "pass"){
-		return done({correlationId: value.correlationId, replyTo: value.replyTo, data: {status: "SUCCESS", type:"madmin", username: data.username}});
-	}
-	return done({correlationId: value.correlationId, replyTo: value.replyTo, data: {status: "FAILURE"}});
+	var username = data.username;
+	var password = data.password;
+	if(adminusername == username && adminpassword == password)
+		return done({correlationId: value.correlationId, replyTo: value.replyTo, data: {status: "SUCCESS", type: "admin"}});
+
+	getConnection(function(err, conn){
+		let query = "select password from users where username=" + mysql.escape(username) + ";";
+		conn.query(query, function(err, results){
+			conn.release();
+			if(results.length!=0){
+				if(results[0].password.length <= 10){
+					if(password == results[0].password)
+						return done({correlationId: value.correlationId, replyTo: value.replyTo, data: {status: "SUCCESS", type: "user"}});
+
+					comparePassword(password, results[0].password, function(err, ismatch){
+						if(ismatch)
+							return done({correlationId: value.correlationId, replyTo: value.replyTo, data: {status: "SUCCESS", type: "user"}});
+					});
+					return done({correlationId: value.correlationId, replyTo: value.replyTo, data: {status: "FAILURE"}});
+				}
+			}
+			//check in mongo
+			getMongodb(function(mongodb){
+				var moviehall = mongodb.collection("moviehall");
+				moviehall.find({ username }).toArray(function(err, ele){
+					if(ele.length == 0)
+						return done({correlationId: value.correlationId, replyTo: value.replyTo, data: {status: "FAILURE"}});
+
+					if(password == ele[0].password)
+						return done({correlationId: value.correlationId, replyTo: value.replyTo, data: {status: "SUCCESS", type:"madmin"}});
+
+					comparePassword(password, ele[0].password, function(err, ismatch){
+						if(ismatch)
+							return done({correlationId: value.correlationId, replyTo: value.replyTo, data: {status: "SUCCESS", type: "madmin"}});
+					});
+					return done({correlationId: value.correlationId, replyTo: value.replyTo, data: {status: "FAILURE"}});
+				});
+			});
+		});
+	});
 }
 
 module.exports = loginMethods;
