9bbdd9e1f1e8e7689cda0c84ec5c27797c58f915
samliweisen <liweisen007@gmail.com>
2018-04-29 18:38:41 +0000

    add movie hall to movie page

--- client/src/components/homepage/MoviePage.js
+++ client/src/components/homepage/MoviePage.js
@@ -9,7 +9,7 @@ export default class MoviePage extends React.Component {
             <div className="container">
                 <MainNav />
                 <div className="page">
-                    <div className="row">
+                    <div className="row movie-info">
                         <div className="col-4">
                             <img className="img-fluid" src="https://cdn.images.express.co.uk/img/dynamic/36/590x/secondary/Avengers-Infinity-War-ends-with-many-of-these-characters-dead-1322280.jpg" />
                         </div>
@@ -18,6 +18,19 @@ export default class MoviePage extends React.Component {
                             <p>Release Date: 2018-04-25</p>
                         </div>
                     </div>
+                    <h2>Buy Tickets</h2>
+                    { [1,2,3,4].map((h) => 
+                    <div className="row movie-hall">
+                        <div className="col-4">
+                            Hall Name
+                        </div>
+                        <div className="col-8">
+                            {[1,2,3,4,5].map((t) => 
+                                <span className="movie-time">14:00pm</span>
+                            )}
+                        </div>
+                    </div>
+                    )}
                 </div>
             </div>
         );
