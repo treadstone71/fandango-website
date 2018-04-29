6f97b63c037b3d8e75db083518af8c618968d18e
kateleecee <cynthialee.lee525@gmail.com>
2018-04-29 03:33:17 -0700

    fix insert user into mysql

--- server/node_backend/users.js
+++ server/node_backend/users.js
@@ -4,33 +4,11 @@ var getMongodb = require('./database/mongodb.js');
 var mysql = require('mysql'); 
 var bcrypt = require('bcrypt');
 
-function genUserId(){
-	//get last of user record from mysql
-	let query = "SELECT userid FROM users ORDER BY userid DESC LIMIT 1";
-	getConnection(async function(err, conn){
-		if(err){
-			return done({correlationId: value.correlationId, replyTo: value.replyTo, data: {status: "FAILURE"}});
-		};
-
-		try
-		{
-			let result = await conn.query(query);
-			conn.release();
-			return result[0];
-		}catch(err){
-			return null;
-		}
-	});
-}
-
 userMethods.register_user = function(value, done){
 	console.log("inside req for adding user", value.data);
-	let query = "INSERT INTO users (userid, email, password) VALUES ?";
-	let userid = genUserId();
-	userid += 1;
+	let query = "INSERT INTO users (userid, email, password) VALUES (?, ?, ?)";
 	//ecrypt password
 	let pwd = bcrypt.hashSync(value.data.password, 10);
-	let data_array = [userid, value.data.email, pwd];
 
 	//DB insert user record
 	getConnection(function(err,conn){
@@ -38,15 +16,18 @@ userMethods.register_user = function(value, done){
 			return done({correlationId: value.correlationId, replyTo: value.replyTo, data: {status: "FAILURE"}});
 		};
 
-		conn.query(query, [data_array], function(err, results){
-			conn.release();
+		let userid = -1;
 
+		conn.query(query, [value.data.id, value.data.email, pwd], function(err, results){
+			conn.release();
 			if (err) {
+				console.log("Insert error "+err);
 				return done({correlationId: value.correlationId, replyTo: value.replyTo, data: {status: "FAILURE"}});
 			}
-			console.log("Number of records inserted: " + result.affectedRows);
+			console.log("Number of records inserted: " + results.affectedRows);
 			return done({correlationId: value.correlationId, replyTo: value.replyTo, data: {status: "SUCCESS", method: "REGISTER"}});
 		});
+
 	});
 	
 }
