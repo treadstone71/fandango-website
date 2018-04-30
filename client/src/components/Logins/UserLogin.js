cbc346c238f85f1ef8e44df1280c2dd63fe32c08
Jiheng Lu <jihenglu@hotmail.com>
2018-04-29 23:05:45 -0700

    fix typo

--- client/src/components/Logins/UserLogin.js
+++ client/src/components/Logins/UserLogin.js
@@ -3,41 +3,49 @@ import MainNav from '../../mainNav.js';
 import '../../css/userlogin.css';
 
 export default class UserLogin extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-            user: {
-                username: '',
-                password: ''
-            }
-        };
-    }
-    handleSubmit(e) {
-        e.preventDefault();
-    }
-    render() {
-        return(
-            <div className="container">
-                <MainNav />
-                <div className="userlogin">
-                    <h1>User Login</h1>
-                    <form onSubmit={this.handleSubmit.bind(this)}>
-                        <div className="form-group">
-                            <label htmlFor="username">Username</label>
-                            <input type="text" className="form-control" id="username" autoFocus/>
-                        </div>
-                        <div className="form-group">
-                            <label htmlFor="password">Password</label>
-                            <input type="password" className="form-control" id="password" />
-                        </div>
-                        <button type="submit" className="btn btn-primary">Login</button>
-            <br/>
-            <br/>
-            <p>Don't' have an Account <a href="/signup">Sign Up</a></p>
-
-                    </form>
-                </div>
+  constructor() {
+    super();
+    this.state = {
+      user: {
+        username: '',
+        password: ''
+      }
+    };
+  }
+  handleSubmit(e) {
+    e.preventDefault();
+  }
+  render() {
+    return (
+      <div className="container">
+        <MainNav />
+        <div className="userlogin">
+          <h1>User Login</h1>
+          <form onSubmit={this.handleSubmit.bind(this)}>
+            <div className="form-group">
+              <label htmlFor="username">Username</label>
+              <input
+                type="text"
+                className="form-control"
+                id="username"
+                autoFocus
+              />
             </div>
-        );
-    }
+            <div className="form-group">
+              <label htmlFor="password">Password</label>
+              <input type="password" className="form-control" id="password" />
+            </div>
+            <button type="submit" className="btn btn-primary">
+              Login
+            </button>
+            <br />
+            <br />
+            <p>
+              Create an Account <a href="/signup">Sign Up</a>
+            </p>
+          </form>
+        </div>
+      </div>
+    );
+  }
 }
