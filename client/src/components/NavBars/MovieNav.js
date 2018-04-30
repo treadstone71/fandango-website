53c8f274c9de956c0ba587bb8f878e48b083f8c7
Sasank <sasank196@gmail.com>
2018-04-29 17:33:05 -0700

    Added paths for Movie admin

---
+++ client/src/components/NavBars/MovieNav.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { apiActions } from '../../apiActions/api.actions.js';
+
+class MovieNav extends React.Component{
+    constructor(){
+        super();
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick(e){
+        const { name } = e.target;
+        const { dispatch } = this.props;
+
+        if(name == "logout"){
+            dispatch(apiActions.logout());
+        }
+
+    }
+
+    render(){
+        return(
+            <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" id="mainNav">
+            <div class="container">
+            <a class="navbar-brand js-scroll-trigger" href="/"><img src="https://lh3.googleusercontent.com/gAuQ6y9UBh5E3AlJL8m383NAcoI4Gl9lM3JosyAwNfqEaj1CdoqTC8bSkymghrbY3iEa9g=s170" /></a>
+            &nbsp;
+    <a class="navbar-brand" href="/madmin/dashboard">Dashboard</a>
+            &nbsp;
+    <ul class="nav navbar-nav actions ml-auto">
+            <li class="dropdown">
+            <a class="btn btn-primary dropdown-toggle " href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown">
+            Movie Admin
+            </a>
+            <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="/admin/add-movie-hall">Add Movie </a></li>
+        <li><a class="dropdown-item" href="/admin/search-movie-hall">Search Movie/Hall</a></li>
+        </ul>
+        </li>
+        </ul>
+        &nbsp;
+    <button class="btn btn-warning navbar-btn navbar-right" name="logout" onClick={this.handleClick}>Log out</button>
+        </div>
+        </nav>
+    );
+    }
+}
+export default MovieNav;
